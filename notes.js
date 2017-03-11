d679c0da87df40d2075810f411be452a132c09d6
webbdm <gwebb762@gmail.com>
2017-03-11 14:02:22 -0600

    refactored entire project

---
+++ notes.js
@@ -0,0 +1,37 @@
+function discount(season){
+  var newProducts = products;
+  for(i=0; i < newProducts.length; i++){
+    switch(season){
+      case "Winter":
+      if(newProducts[i].category_id === 1){
+        newProducts[i].price = (newProducts[i].price - (newProducts[i].price * categories[0].discount)).toFixed(2);}
+        break; 
+      case "Autumn":
+      if(newProducts[i].category_id === 2){
+        newProducts[i].price = (newProducts[i].price - (newProducts[i].price * categories[1].discount)).toFixed(2);}
+        break;
+      case "Spring":
+      if(newProducts[i].category_id === 3){
+        newProducts[i].price = (newProducts[i].price - (newProducts[i].price * categories[2].discount)).toFixed(2);}
+        break;
+      default:
+        console.log('default');
+     }
+   }
+  
+  renderProducts(newProducts);
+}
+
+switch(season){
+  case 'winter':
+    for(var i=0; i<categories.length; i++){
+      if(categories[i].season_discount === 'winter'){
+        var discount = categories[i].discount; ///0.54
+        var category = categories[i].id;
+        if(newProduct[i].category_id === category)
+        newProduct[i].price -= newProduct[i].price*discount;
+      }
+    }
+  case 'summer':
+  case 'fall':
+
---
+++ notes.md
@@ -0,0 +1,13 @@
+```function renderProducts(){
+  
+}//Product DOM
+function loadedProducts(){
+  products = data
+  renderProducts();
+} //json sucess calls
+function loadedCats(){
+  categories = data
+} //json sucess calls
+//categoriesXHR call
+//ProductsXHR call
+ ```
